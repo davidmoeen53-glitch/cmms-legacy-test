0c4036810bae7cc3c0bb13dbd1f525961a2a8ea1
davidmoeen53-glitch <davidmoeen53@gmail.com>
2026-09-23 08:35:44 +0300

    Load Rails secret token from environment

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,9 @@
-# Be sure to restart your server when you modify this file.
+# Secret used to verify signed cookies.
+# Vercel/test deployments must provide SECRET_TOKEN as an environment variable.
+secret_token = ENV['SECRET_TOKEN']
 
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Cmms::Application.config.secret_token = 'f4f8e8c3fbe156b1b1773c56bc38d0e63a9888a4d31e07922e477d8fca47a0cb7a0d14228d07955ed289d850b43030bd542cd0bb2ec2e10858b046a9ef60fa05'
+if Rails.env.production? && (secret_token.nil? || secret_token.length < 64)
+  raise 'SECRET_TOKEN must be set to a random value of at least 64 characters'
+end
+
+Cmms::Application.config.secret_token = secret_token || 'development-only-secret-token-change-me-1234567890'
